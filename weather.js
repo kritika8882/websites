911f068ef8329cea624d15d23bda330186c4f045
kritika8882 <kritika8882@gmail.com>
2026-09-02 13:39:18 +0530

    Current_Weather

---
+++ weather.js
@@ -0,0 +1,37 @@
+const API_KEY = `abf20c46d6764d1baf995527263108`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+
+const getWeather = async (city) => {
+    weather.innerHTML=`<h2>Loading....</h2>`
+    const url = `http://api.weatherapi.com/v1/current.json?key=abf20c46d6764d1baf995527263108&q=${city}&aqi=yes`
+    const response = await fetch(url);
+    const data = await response.json();
+    return showWeather(data)
+}
+
+const showWeather = (data) =>{
+    if(data.code== "1006"){
+        weather.innerHTML = `<h2> No matching location found.</h2>`
+        return;
+    }    
+    weather.innerHTML = `
+        <div>
+            <img src=${data.current.condition.icon} " alt="" style="height: 90px; width: 90px; margin-right: 20px;">
+        </div>
+        <div>
+            <h2>${data.current.temp_c}&deg;C</h2>
+            <h4>${data.current.condition.text}</h4>
+        </div>
+        `
+
+    
+    
+}
+
+form.addEventListener("submit", (event) => {
+    getWeather(search.value)
+    event.preventDefault();
+}
+)
